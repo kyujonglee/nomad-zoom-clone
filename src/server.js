e3da7786e07e885b27661268b12b407fc03b1d62
이규종 <kyujong93@naver.com>
2021-11-04 11:26:57 +0900

    Create WebSocket Server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import http from 'http';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 
@@ -9,10 +11,15 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => {
   console.log(`✅ server starting PORT:${PORT}`);
 };
 
-app.listen(PORT, handleListen);
+// app.listen(PORT, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+server.listen(PORT, handleListen);
