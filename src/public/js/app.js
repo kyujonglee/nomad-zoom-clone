dc4f82b258d5af8a596088f64bc18e399e20b2a5
이규종 <kyujong93@naver.com>
2022-01-21 04:40:12 +0900

    [Edit] public stun server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -146,7 +146,13 @@ const makeConnection = () => {
   myPeerConnection = new RTCPeerConnection({
     iceServers: [
       {
-        urls: ['stun.xs4all.nl:3478', 'stun.xten.com:3478'],
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ],
       },
     ],
   });
